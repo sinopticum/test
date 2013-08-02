66867535240f8b678ce4f3166d3192f46c388f47
Renovatio <Renovatio@Renovatios-Mac-mini.local>
2013-08-02 11:51:45 +0300

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Renovatio on 8/2/13.
+//  Copyright (c) 2013 Renovatio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RSAppDelegate class]));
+    }
+}
---
+++ testTests/testTests.m
@@ -0,0 +1,36 @@
+//
+//  testTests.m
+//  testTests
+//
+//  Created by Renovatio on 8/2/13.
+//  Copyright (c) 2013 Renovatio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testTests : XCTestCase
+
+@end
+
+@implementation testTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
